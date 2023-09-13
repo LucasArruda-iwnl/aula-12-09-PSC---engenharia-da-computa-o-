299ea330f7feb3e5e903d28a6a957716111f92aa
Lucas Arruda <105546865+LucasArruda-iwnl@users.noreply.github.com>
2023-09-12 21:33:43 -0300

    Add files via upload

---
+++ exercicio2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class exercicio2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); 
+        double n = sc.nextDouble(); 
+        double e = 1;
+        for (int i = 1; i <= n; i++) { 
+            double fatorial = 1;
+            for (int j = 2; j <= i; j++){ 
+
+                fatorial *= j;
+            }
+            e = e + 1/fatorial(i);
+
+         }
+         System.out.println(i);
+         sc.close();
+    }
+    
+}
